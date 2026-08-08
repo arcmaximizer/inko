5fc4167fc193c2ba8c17e7ccf9d5eb04cb35e838
Arc <arcmaximizer@proton.me>
2026-08-08 16:08:16 +0100

    chore: fix comment inaccuracy

--- migrations/0004_add_login_and_sessions.sql
+++ migrations/0004_add_login_and_sessions.sql
@@ -1,6 +1,6 @@
 -- Migration number: 0004 	 2026-08-04T15:14:50.057Z
 
--- id will be a randomly generated 48-byte value uuid thingy
+-- id will be a randomly generated value uuid thingy
 CREATE TABLE IF NOT EXISTS sessions (id TEXT PRIMARY KEY NOT NULL, created TIMESTAMP NOT NULL);
 
 -- KV now
